60792ca6a32ad69dc6612ad862f1773623392736
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-05-04 12:04:49 +0200

    Initial commit

---
+++ MetalShaders/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetalShaders
+//
+//  Created by Davide Castaldi on 04/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetalShaders/MetalShadersApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalShadersApp.swift
+//  MetalShaders
+//
+//  Created by Davide Castaldi on 04/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MetalShadersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
